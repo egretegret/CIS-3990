40b895d535819193992071d09f79d65502022bef
egretegret <egret@seas.upenn.edu>
2023-04-02 17:32:06 -0400

    Add button functionality

--- da 5/apostrophe/src/components/Poem.js
+++ da 5/apostrophe/src/components/Poem.js
@@ -3,16 +3,30 @@ import React, { useState } from 'react';
 import { Arrow } from './Arrow';
 import { Bookmark } from './Bookmark';
 
-export function Poem({title, author, poem}) {
+export function Poem({ title, author, poem }) {
     const [isClosed, setIsClosed] = useState(true);
+    const [isSaved, setIsSaved] = useState(false);
+
+    function toggleClose() {
+        isClosed ? setIsClosed(false) : setIsClosed(true);
+    }
+
+    function toggleSave() {
+        isSaved ? setIsSaved(false) : setIsSaved(true);
+    }
+
     return (
         <div className={`Poem ${isClosed ? "closed" : ""}`}>
             <h3>{title}</h3>
             <h4>BY {author}</h4>
             <p>{poem}</p>
             <div className="action-bar">
-                <Arrow action={setIsClosed}/>
-                <Bookmark />
+                <div className="button" onClick={toggleClose} >
+                    <Arrow open={!isClosed} />
+                </div>
+                <div className="button" onClick={toggleSave} >
+                    <Bookmark saved={isSaved} />
+                </div>
             </div>
         </div>
     );
