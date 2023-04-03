8732f8475d27bc86791c58e5e2261c2f225beab3
egretegret <egret@seas.upenn.edu>
2023-04-03 03:22:58 -0400

    add home view toggle

--- da 5/apostrophe/src/Home.js
+++ da 5/apostrophe/src/Home.js
@@ -1,14 +1,30 @@
+import React, { useState } from 'react';
+
 import './styles/Home.css'
 import { Poem } from './components/Poem';
-import { poems } from './data';
+import { discoverPoems, friendsPoems } from './data';
 
 
 export function Home() {
+    const [active, setActive] = useState("discover");
+
     return (
         <div className='Home'>
-            {poems.map(p => <Poem title={p.title}
-                author={p.author}
-                poem={p.poem} />)}
+            <h1>A'</h1>
+            <div className="scroll-top"></div>
+            <div className="discover-friends">
+                <h2 className={`${active == "discover" ? "active" : ""}`} onClick={(e) => setActive(e.target.innerText)}>discover</h2>
+                <h2 className={`${active == "friends" ? "active" : ""}`} onClick={(e) => setActive(e.target.innerText)}>friends</h2>
+            </div>
+            <div className="divider"></div>
+            <div className="divider-bottom"></div>
+            {active == "discover" ?
+                discoverPoems.map(p => <Poem title={p.title}
+                    author={p.author}
+                    poem={p.poem} />) :
+                friendsPoems.map(p => <Poem title={p.title}
+                    author={p.author}
+                    poem={p.poem} />)}
         </div>
 
     );
--- da 5/apostrophe/src/data.js
+++ da 5/apostrophe/src/data.js
@@ -1,4 +1,4 @@
-export let poems = [
+export let discoverPoems = [
     {
         "title": "Catch",
         "author": "SAMIYA BASHIR",
@@ -51,5 +51,205 @@ export let poems = [
         \n \
         I'll be counting the days and nights till we're together again.\n \
         Until then, it's all my love that I send!",
+    },
+    {
+        "title": "i traveled the world. it was fine.",
+        "author": "SAMIYA BASHIR",
+        "poem": "::  lists  ::\n \
+        \n \
+        genres\n \
+        not-genres\n \
+        \n \
+        survival\n \
+        surveillance\n \
+        \n \
+        \n \
+        \n \
+        ::   lists  ::\n \
+        \n \
+        bath salts\n \
+        meds\n \
+        nail stuff\n \
+        grapefruit juice\n \
+        keys\n \
+        protein\n \
+        tequila\n \
+        other keys\n \
+        gin\n \
+        grapefruit juice\n \
+        other other keys\n \
+        hair\n \
+        \n \
+        \n \
+        \n \
+        ::   lists   ::\n \
+        \n \
+        things i won’t be answering:\n \
+        emails\n \
+        voice mails\n \
+        really any mail without a stamp\n \
+        phone calls\n \
+        call outs\n \
+        call ins\n \
+        ungrounded theories\n \
+        anything that begins “can i touch...”\n \
+        \n \
+        \n \
+        \n \
+        ::  states  ::\n \
+        \n \
+        potentially\n \
+        pointless\n \
+        \n \
+        surveillance\n \
+        survival\n \
+        \n \
+        \n \
+        \n \
+        ::  states  ::\n \
+        \n \
+        selfish\n \
+        she invites\n \
+        all the curses\n \
+        (no curse for you!)\n \
+        \n \
+        \n \
+        \n \
+        ::  states  ::\n \
+        \n \
+        how are we all so busy now\n \
+        again\n \
+        \n \
+        \n \
+        \n \
+        ::  lists  ::\n \
+        \n \
+        my name\n \
+        the way my name\n \
+        is said\n \
+        \n \
+        \n \
+        \n \
+        yawn",
+    }
+];
+
+export let friendsPoems = [
+    {
+        "title": "Catch",
+        "author": "SAMIYA BASHIR",
+        "poem": "if this is a game then we have made it, unknowing, \n \
+      to the final four. unlikely underdogs. spectators turned \n \
+      to suspect sport. anti-athletes. out of shape beyond reason. \n \
+      \n \
+      at season’s height we fight for a limited audience. few dancers. \n \
+      fewer cheers. down by 30 and our coach m.i.a. we, foolish, dribble. \n \
+      each bounce-back brings a stranger. can’t call us for traveling because \n \
+      \n \
+      we ain’t going nowhere. instead, we trade terrified looks. search\n \
+      for the pass but no one stays open for long. even if we knew what to do\n \
+      to pull this through we’ve got two other teams waiting, impatient, to take us out.\n \
+      ",
+    },
+    {
+        "title": "Without you",
+        "author": "DAWN",
+        "poem": "All these days and nights without you here,\n \
+        It's a little bit more than I can bear.\n \
+        \n \
+        The days are cold and so very long.\n \
+        I don't know how much longer I can be strong.\n \
+        \n \
+        My nights are so lonely and sad.\n \
+        Sometimes I can't help but get mad. \n \
+        \n \
+        I see your face everywhere I look.\n \
+        It breaks my heart that my great love is what they took.\n \
+        \n \
+        Without you here it feels like the end.\n \
+        I'm lost without you 'cause you were also my best friend.\n \
+        \n \
+        I'll be counting the days and nights till we're together again.\n \
+        Until then, it's all my love that I send!",
+    },
+    {
+        "title": "i traveled the world. it was fine.",
+        "author": "SAMIYA BASHIR",
+        "poem": "::  lists  ::\n \
+        \n \
+        genres\n \
+        not-genres\n \
+        \n \
+        survival\n \
+        surveillance\n \
+        \n \
+        \n \
+        \n \
+        ::   lists  ::\n \
+        \n \
+        bath salts\n \
+        meds\n \
+        nail stuff\n \
+        grapefruit juice\n \
+        keys\n \
+        protein\n \
+        tequila\n \
+        other keys\n \
+        gin\n \
+        grapefruit juice\n \
+        other other keys\n \
+        hair\n \
+        \n \
+        \n \
+        \n \
+        ::   lists   ::\n \
+        \n \
+        things i won’t be answering:\n \
+        emails\n \
+        voice mails\n \
+        really any mail without a stamp\n \
+        phone calls\n \
+        call outs\n \
+        call ins\n \
+        ungrounded theories\n \
+        anything that begins “can i touch...”\n \
+        \n \
+        \n \
+        \n \
+        ::  states  ::\n \
+        \n \
+        potentially\n \
+        pointless\n \
+        \n \
+        surveillance\n \
+        survival\n \
+        \n \
+        \n \
+        \n \
+        ::  states  ::\n \
+        \n \
+        selfish\n \
+        she invites\n \
+        all the curses\n \
+        (no curse for you!)\n \
+        \n \
+        \n \
+        \n \
+        ::  states  ::\n \
+        \n \
+        how are we all so busy now\n \
+        again\n \
+        \n \
+        \n \
+        \n \
+        ::  lists  ::\n \
+        \n \
+        my name\n \
+        the way my name\n \
+        is said\n \
+        \n \
+        \n \
+        \n \
+        yawn",
     }
 ];
